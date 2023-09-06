5976458b04238d269f6ba302a4cb20e999bb33e0
Saad <1905090@ugrad.cse.buet.ac.bd>
2023-09-07 05:27:16 +0600

    order place done

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -14,7 +14,7 @@ function RestaurantCard(props) {
         let myStyle={};
         if(props.data.restaurant_image){
             myStyle={
-                backgroundImage:`url("https://images.getbento.com/accounts/0dd435255ed47ef59b07b494968680e4/media/images/12960PQ_CHICAGO_INTERIOR_FULL.jpg?w=1200&fit=crop&auto=compress,format&crop=focalpoint&fp-x=0.5&fp-y=0.5")`
+                backgroundImage:"url(" + props.data.restaurant_image + ")"
             };
         }
         
